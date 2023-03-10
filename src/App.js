df9b04aab27a6027b33a06af79eb7df64235d8c5
hamzashahab1610 <hamzashahab1610@gmail.com>
2023-03-10 16:23:55 +0500

    feat: add redirection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./pages/login";
 import SignUp from "./pages/signup";
+import { useEffect } from "react";
 
 function App() {
+	const navigate = useNavigate();
+	useEffect(() => {
+		if (window.location.pathname === "/") navigate("/login");
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	return (
 		<div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
 			<Routes>
